1bad8981c36475a03d79f3504199f5d63821aaa3
Edison Javier Colorado Mejia <edy89@users.noreply.github.com>
2023-01-19 21:22:53 -0500

    Add files via upload

--- handler.js
+++ handler.js
@@ -1,14 +1,56 @@
 'use strict';
 
-module.exports.hello = (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello World"
-      },
-      null,
-      2
-    ),
-  };
-}
+module.exports.hello = (event, context, callback) => {
+	console.log(event);
+
+	var message = 'HELLO WORLD';
+	const name = event.queryStringParameters && event.queryStringParameters.name;
+
+	if (name !== null) {
+		message = 'Hello ' + name;
+	}
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: message,
+			input: event
+		})
+	};
+
+	callback(null, response);
+};
+
+module.exports.moi = (event, context, callback) => {
+	var message = 'HELLO WORLD';
+
+	const name = event.pathParameters && event.pathParameters.name;
+
+	if (name !== null) {
+		message = 'Hello ' + name;
+	}
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: message,
+			input: event
+		})
+	};
+
+	callback(null, response);
+};
+
+module.exports.hola = (event, context, callback) => {
+	const body = JSON.parse(event.body);
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: body,
+			input: event
+		})
+	};
+
+	callback(null, response);
+};
